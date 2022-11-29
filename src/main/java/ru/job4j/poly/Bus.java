08d87fde25589d80a8cbb1f40e5ddf5e822dde25
Lesa-89 <lesa-89@mail.ru>
2022-11-29 22:06:45 +0300

    3. Интерфейс Input [#181092]

---
+++ src/main/java/ru/job4j/poly/Bus.java
@@ -0,0 +1,18 @@
+package ru.job4j.poly;
+
+public class Bus implements Transport {
+    @Override
+    public void drive() {
+
+    }
+
+    @Override
+    public void passengers(int passengersCount) {
+
+    }
+
+    @Override
+    public int refuel(int gas) {
+        return 0;
+    }
+}
