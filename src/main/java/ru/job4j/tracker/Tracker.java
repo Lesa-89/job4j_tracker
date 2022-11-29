94b83332a0eada53e1b1e64df420d3ba62de8104
Lesa-89 <lesa-89@mail.ru>
2022-11-29 16:27:40 +0300

    5. Tracker - хранилище [#396 #365554]

--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -14,27 +14,19 @@ public class Tracker {
     }
 
     public Item[] findAll() {
-        Item[] rsl = new Item[items.length];
-        int size = 0;
-        for (int i = 0; i < items.length; i++) {
-            if (items[i] != null) {
-                rsl[size] = items[i];
-                size++;
-            }
-        }
-        return Arrays.copyOf(rsl, size);
+        return Arrays.copyOf(items, size);
     }
 
     public Item[] findByName(String key) {
-        Item[] rsl = findAll();
-        int size = 0;
-        for (int i = 0; i < rsl.length; i++) {
+        Item[] rsl = new Item[size];
+        int counter = 0;
+        for (int i = 0; i < size; i++) {
             if (items[i].getName().equals(key)) {
-                rsl[size] = items[i];
-                size++;
+                rsl[counter] = items[i];
+                counter++;
             }
         }
-        return Arrays.copyOf(rsl, size);
+        return Arrays.copyOf(rsl, counter);
     }
 
     public Item findById(int id) {
