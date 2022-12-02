b2d2cdd1c9a0fd0c69604e3437551fe3b6fabe37
Lesa-89 <lesa-89@mail.ru>
2022-12-03 00:26:41 +0300

    0.5. Иерархия исключений и множественный catch. [#219368]

--- src/main/java/ru/job4j/ex/FindEl.java
+++ src/main/java/ru/job4j/ex/FindEl.java
@@ -1,5 +1,6 @@
 package ru.job4j.ex;
 
+import java.util.Arrays;
 import java.util.Objects;
 
 public class FindEl {
@@ -12,18 +13,43 @@ public class FindEl {
             }
         }
         if (rsl == -1) {
-            throw new ElementNotFoundException();
+            throw new ElementNotFoundException("ElementNotFound");
         }
         return rsl;
     }
 
-    public static void main(String[] args) {
-        String[] test = new String[0];
-        String msg = "";
-        try {
-            indexOf(test, msg);
-        } catch (ElementNotFoundException e) {
-            e.printStackTrace();
+    public static boolean sent(String value, String[] abuses) throws ElementNotFoundException {
+        if (Arrays.asList(abuses).contains(value)) {
+            throw new ElementAbuseException("ElementAbuse");
         }
+        return true;
+    }
+
+    public static void process(String[] values, String key, String[] abuses) {
+        try {
+            if (indexOf(values, key) != -1) {
+                sent(key, abuses);
+            }
+        } catch (ElementAbuseException ea) {
+            ea.printStackTrace();
+        } catch (ElementNotFoundException en) {
+            en.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        } catch (Throwable th) {
+            th.printStackTrace();
+        }
+    }
+
+    public static void main(String[] args) {
+        String[] testFirst = new String[]{"A", "B"};
+        String[] testSecond = new String[0];
+        String msg = "A";
+        process(testFirst, msg, testSecond);
+        String[] testFirstEx = new String[0];
+        String[] testSecondEx = new String[0];
+        String msgEx = "";
+        process(testFirstEx, msgEx, testSecondEx);
+
     }
 }
---
+++ src/main/java/ru/job4j/ex/UserStore.java
@@ -0,0 +1,41 @@
+package ru.job4j.ex;
+
+public class UserStore {
+
+    public static User findUser(User[] users, String login) throws UserNotFoundException {
+        User result = null;
+        for (User checkingUser : users) {
+            if (checkingUser.getUsername().equals(login)) {
+                result = checkingUser;
+                break;
+            }
+        }
+        if (result == null) {
+            throw new UserNotFoundException("UserNotFound");
+        }
+        return result;
+    }
+
+    public static boolean validate(User user) throws UserInvalidException {
+        if (!user.isValid() || user.getUsername().length() < 3) {
+            throw new UserInvalidException("UserInvalid");
+        }
+        return true;
+    }
+
+    public static void main(String[] args) throws UserNotFoundException {
+        User[] users = {
+                new User("Petr Arsentev", true)
+        };
+        User user = findUser(users, "Petr Arsentev");
+        try {
+            if (validate(user)) {
+                System.out.println("This user has an access");
+            }
+        } catch (UserInvalidException ei) {
+            throw new UserInvalidException("пользователь не валидный");
+        } catch (UserNotFoundException enf) {
+            throw new UserNotFoundException("пользователь не найден");
+        }
+    }
+}
