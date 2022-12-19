017714df9dd5d26fe7df1b9459fed1a3777ba2e3
Lesa-89 <lesa-89@mail.ru>
2022-12-20 00:28:28 +0300

    Revert "3. Генерация документации по JavaDoc [#313880]"
    This reverts commit a0479f8f62de42c9d510d3ae5a0003d9eb5b9cc9.

---
+++ src/test/java/ru/job4j/ex/FactTest.java
@@ -0,0 +1,27 @@
+package ru.job4j.ex;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.junit.jupiter.api.Assertions.*;
+
+class FactTest {
+
+    @Test
+    public void whenException() {
+        IllegalArgumentException exception = assertThrows(
+                IllegalArgumentException.class,
+                () -> {
+                    new Fact().calc(-1);
+                });
+        assertThat(exception.getMessage()).isEqualTo("N could not be less then 0");
+    }
+
+    @Test
+    public void when5then120() {
+        int expected = 120;
+        int n = 5;
+        int result = new Fact().calc(n);
+        assertThat(expected).isEqualTo(result);
+    }
+}
