2f61725cc21888fa63f3448b857b261931947e84
Lesa-89 <lesa-89@mail.ru>
2023-01-09 22:19:30 +0300

    2. Отсортировать департаменты [#34352 #378933]

--- src/main/java/ru/job4j/collection/DepDescComp.java
+++ src/main/java/ru/job4j/collection/DepDescComp.java
@@ -3,11 +3,11 @@ package ru.job4j.collection;
 import java.util.Comparator;
 
 public class DepDescComp implements Comparator<String> {
-    @SuppressWarnings("checkstyle:WhitespaceAround")
     @Override
     public int compare(String o1, String o2) {
         String[] o1s = o1.split("/");
         String[] o2s = o2.split("/");
-        return o2s[0].equals(o1s[0]) ? o1.compareTo(o2) : o2.compareTo(o1);
+        int result = o2s[0].compareTo(o1s[0]);
+        return result == 0 ? o1.compareTo(o2) : result;
     }
 }
