ac542b2c7db319c9476e4fede513d0e7cb251fd9
Lesa-89 <lesa-89@mail.ru>
2023-02-04 21:03:54 +0300

    17. Тестовое задание из модуля коллекции Lite переделать на Stream API. [#24260]

--- src/main/java/ru/job4j/bank/BankService.java
+++ src/main/java/ru/job4j/bank/BankService.java
@@ -8,6 +8,7 @@ import java.util.Map;
 /**
  * Класс описывает работу банковского сервиса
  * с простейшими операциями с пользователями и их счетами
+ *
  * @author SERGEY LARICHEV
  * @version 1.0
  */
@@ -20,6 +21,7 @@ public class BankService {
 
     /**
      * Метод принимает пользователя и добавляет его в базу если его там нет.
+     *
      * @param user пользователь который добавляется в базу
      */
     public void addUser(User user) {
@@ -28,18 +30,20 @@ public class BankService {
 
     /**
      * Метод удаляет пользователя из базы если он там есть.
+     *
      * @param passport номер паспорта пользователя
      * @return возвращает true если пользователь удален и false если нет
      */
     public boolean deleteUser(String passport) {
-        return  users.remove(new User(passport, "")) != null;
+        return users.remove(new User(passport, "")) != null;
     }
 
     /**
      * Метод ищет пользователя по номеру паспорта, и если такой пользователь есть,
      * добавляет ему указанный счет
+     *
      * @param passport номер паспорта пользователя
-     * @param account счет для добавления
+     * @param account  счет для добавления
      */
     public void addAccount(String passport, Account account) {
         User user = findByPassport(passport);
@@ -53,50 +57,46 @@ public class BankService {
 
     /**
      * Метод ищет пользователя по номеру паспорта
+     *
      * @param passport номер паспорта пользователя
      * @return возвращает пользователя если он найден или null если нет
      */
     public User findByPassport(String passport) {
-        User res = null;
-        for (User user : users.keySet()) {
-            if (passport.equals(user.getPassport())) {
-                res = user;
-                break;
-            }
-        }
-        return res;
+        return users.keySet()
+                .stream()
+                .filter(user -> user.getPassport().equals(passport))
+                .findFirst().
+                orElse(null);
     }
 
     /**
      * Метод ищет счет по номеру паспорта и реквизитам счета
-     * @param passport номер паспорта пользователя
+     *
+     * @param passport  номер паспорта пользователя
      * @param requisite реквизиты счета
      * @return возвращает счет если он найден или null если нет
      */
     public Account findByRequisite(String passport, String requisite) {
         User user = findByPassport(passport);
-        Account res = null;
         if (user != null) {
-            List<Account> list = getAccounts(user);
-            for (Account account : list) {
-                if (requisite.equals(account.getRequisite())) {
-                    res = account;
-                    break;
-                }
-
-            }
+            return users.get(user).
+                    stream().
+                    filter(acc -> acc.getRequisite().equals(requisite)).
+                    findFirst().
+                    orElse(null);
         }
-        return res;
+        return null;
     }
 
     /**
      * Метод производит поиск счетов отправителя и получателя по номеру паспорта и реквизитам
      * и осуществляет перевод средств с счета отправителя на счет получателя
-     * @param srcPassport номер паспорта отправителя
-     * @param srcRequisite реквизиты счета отправителя
-     * @param destPassport номер паспорта получателя
+     *
+     * @param srcPassport   номер паспорта отправителя
+     * @param srcRequisite  реквизиты счета отправителя
+     * @param destPassport  номер паспорта получателя
      * @param destRequisite реквизиты счета получателя
-     * @param amount сумма перевода
+     * @param amount        сумма перевода
      * @return возвращает true если перевод удался и false если нет
      */
     public boolean transferMoney(String srcPassport, String srcRequisite,
@@ -114,6 +114,7 @@ public class BankService {
 
     /**
      * Метод возвращает список счетов пользователя
+     *
      * @param user пользователь
      * @return возвращает список счетов пользователя
      */
